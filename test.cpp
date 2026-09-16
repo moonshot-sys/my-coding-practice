a5e659273ef69f402d0d91b76669e0877cc6482d
문정훈 <moonshot@ajou.ac.kr>
2026-09-16 23:01:51 +0900

    포인터 개념 학습
    포인터 개념 및 null 확인 및 공부.

--- test.cpp
+++ test.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+//C++와 C언어의 공통 문법 구조.
+void getType(int a) {
+    std::cout<<"int"<<std::endl;
+}
+
+void getType(int* a){
+    std::cout <<"int*"<<std::endl;
+}
+
 int main(){
-    std::cout << "Hello World" << std::endl;
+    int* a;
+    a=nullptr;
+    std::cout<<a<<std::endl;
+
     return 0;
+
 }
