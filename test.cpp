4c3eb30e2e2770e2cb4f1ade873ae056e894eeef
문정훈 <moonshot@ajou.ac.kr>
2026-09-21 20:36:58 +0900

    배열과 포인터 실습

--- test.cpp
+++ test.cpp
@@ -1,29 +1,15 @@
 #include <iostream>
 //C++와 C언어의 공통 문법 구조.
 int main(){
-    int* a1 = new int(10);
-    int* a2 = new int[5];
-    int* a3 = new int[5] {1,2,3,4,5};
+    int arr[3] = {1,2,3};
 
-    std::cout << "a1이 가리키는 주소 값 : " << a1<<std::endl;
-    std::cout << "a2이 가리키는 주소 값 : " << a2 <<std::endl;
-    std::cout<<"a3이 가리키는 주소 값 (heap memory) : " << a3 << std::endl;
-    std::cout<<"a1의 주소값 : " << &a1 << std::endl;
-    std::cout <<"a2의 주소값 : " << &a2<< std::endl;
-    std::cout << "a3의 주소값 (stack memory) : " << &a3 << std::endl;
+    std::cout << "arr (heap) : "<<arr<<std::endl;
+    std::cout << "&arr (stack) : "<<&arr<<std::endl;
+    std::cout << "&arr[0] : " << & arr[0] <<std::endl;
+    std::cout << "&arr[1] : " << & arr[1] <<std::endl;
+    std::cout << "&arr[2] : " << &arr[2] << std::endl;
+    std::cout << "(arr + 1) : "<< (arr+1)<<std::endl;
+    std::cout << "&arr[0] +1 : "<<& arr[0] + 1<<std::endl;
 
-    int n1= 10;
-    int n2 = 20;
-    int* b = &n1;
-    int* c = &n2;
-
-    std::cout <<"b의 주소값(stack memory) : " << b << std::endl;
-    std::cout << "c의 주소값(stack memory) : " << c << std::endl;
-
-    delete a1;
-    delete[] a2;
-    delete[] a3;
-
-    return 0;
-
+    return 0; 
 }
