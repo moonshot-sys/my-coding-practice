bf5e2d2794ca186c28b943e0d7f1b1e8bcc04fed
문정훈 <moonshot@ajou.ac.kr>
2026-09-21 20:49:07 +0900

    포인터 증감 연산
    포인터 증감 연산

--- test.cpp
+++ test.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 //C++와 C언어의 공통 문법 구조.
+//포인터는 주소값을 저장하는 변수이기 때문에 포인터끼리의 사칙 연산 중 뺄셈을 
+//제외한 나머지 연산은 의미가 없다. 뼬셈의 경우 두 포인터 사이의 거리를 나타냄.
+//포인터에 정수 갓을 더하거나 빼는 것(위치 이동)은 가능하지만 float과 같은 실수는 연산할 수 없다.
+//[] 연산자가 포인터의 엇셈과 동일한 역할을 한다. 즉 arr[1] 은 arr+1과 동일한 역할
 int main(){
-    int arr[3] = {1,2,3};
+    int arr[4] ={1,2,3,4};
+    int* p = arr;
 
-    std::cout << "arr (heap) : "<<arr<<std::endl;
-    std::cout << "&arr (stack) : "<<&arr<<std::endl;
-    std::cout << "&arr[0] : " << & arr[0] <<std::endl;
-    std::cout << "&arr[1] : " << & arr[1] <<std::endl;
-    std::cout << "&arr[2] : " << &arr[2] << std::endl;
-    std::cout << "(arr + 1) : "<< (arr+1)<<std::endl;
-    std::cout << "&arr[0] +1 : "<<& arr[0] + 1<<std::endl;
+    std::cout<<p+3<<std::endl;
+    std::cout <<&p[3]<<std::endl;
 
-    return 0; 
+    std::cout << *(p+3) << std::endl;
+    std::cout<< p[3] <<std::endl;
+
+    std::cout<<sizeof(arr)<<std::endl;
+    std::cout << sizeof(p) << std::endl;
+
+    return 0;
 }
