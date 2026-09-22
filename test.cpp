ddaa10df0085e225bc87ebfad94f3446aaf7a347
문정훈 <moonshot@ajou.ac.kr>
2026-09-22 22:39:08 +0900

    while 문과 cin
    while 문과 cin

--- test.cpp
+++ test.cpp
@@ -1,26 +1,23 @@
 #include <iostream>
 //C++의 for 문
-void print(){
-int i;
+/*변수는 변수 사용 직전에 선언해도 된다.*/
+/*행운의 숫자 맞추기*/
 
-  for(i=0;i<10;i++){
-    std::cout << i << std::endl;
-
-  }
-  
-}
-
-void sum(){
-    int i, sum = 0;
-    for(i =1; i<=10;i++){
-        sum+=i;
-    }
-
-    std::cout<<"합은 : " <<sum << std::endl;
-   
-}
 int main(){
-    print();
-    sum();
- return 0;
+  int lucky_number = 3;
+  std::cout << "내 비밀의 수를 맞추어 보세요~"<< std::endl;
+
+  int user_input; //사용자 입력
+  while (1) {
+    std::cout <<"입력 : ";
+    std::cin >>user_input; //cin은 사용자로부터 입력을 받아서 >>를 통해 변수에 넣는다.
+                           //cin 또한 std에 정의
+    if(lucky_number == user_input){
+      std::cout<<"맞추셨습니다. "<<std::endl;
+      break;
+    }else{
+      std::cout <<"다시 생각해보세요~" <<std::endl;
+    }
+  }
+  return 0;
 }
